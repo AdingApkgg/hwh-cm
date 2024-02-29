c152d115bd5a8c59c0830f1e89cd080853ed6fab
AdingApkgg <AdingApkgg@outlook.com>
2024-03-01 02:16:34 +0800

    .

--- js/main.js
+++ js/main.js
@@ -1,4 +1,10 @@
+var viewImgList = document.getElementById("viewImgList");
+
 document.getElementById("scroll-down").onclick = function () {
   document.documentElement.scrollTop =
     document.getElementById("viewImgList").offsetTop;
 };
+
+setInterval(function () {
+  viewImgList.scrollTop++;
+}, 20);
